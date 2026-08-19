62a914ff8c1ae1afa5adc00ceae6d7c2e5c4bf05
smoothoperator <smoothoperator@smoothoperators-MacBook-Air.local>
2026-08-19 15:52:28 -0400

    hotfix

---
+++ target/debug/deps/primitive_types3-859f6aca87356843.d
@@ -0,0 +1,9 @@
+/Users/smoothoperator/onepiece/programming/oxide/rustlings/target/debug/deps/primitive_types3-859f6aca87356843.d: exercises/04_primitive_types/primitive_types3.rs Cargo.toml
+
+/Users/smoothoperator/onepiece/programming/oxide/rustlings/target/debug/deps/libprimitive_types3-859f6aca87356843.rmeta: exercises/04_primitive_types/primitive_types3.rs Cargo.toml
+
+exercises/04_primitive_types/primitive_types3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/primitive_types2.d
@@ -0,0 +1,1 @@
+/Users/smoothoperator/onepiece/programming/oxide/rustlings/target/debug/primitive_types2: /Users/smoothoperator/onepiece/programming/oxide/rustlings/exercises/04_primitive_types/primitive_types2.rs
